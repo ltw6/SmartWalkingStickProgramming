2abed818994317ca8323e2f8d2e0c1f8840ecc49
ltw6 <115619436+ltw6@users.noreply.github.com>
2022-11-16 15:49:41 +0000

    Update ArduinoInterfacing.ino
    Basic ESP32/HC-SR05 interfacing

--- ArduinoInterfacing.ino
+++ ArduinoInterfacing.ino
@@ -1,9 +1,25 @@
-void setup() {
-  // put your setup code here, to run once:
+//Variable Initialization
+const int trigPin = 5;
+const int echoPin = 18;
 
+long duration;
+float distanceSm;
+float distanceInch;
+
+//Definitions 
+#define SOUND_SPEED 0.034
+#define CM_TO_INCH 0.393701
+
+void setup() {
+  //Initialize HCSR05 sensor input pins
+  Serial.begin(9600); //Begin serial communication
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin,INPUT);
+  
+ 
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  
 }
