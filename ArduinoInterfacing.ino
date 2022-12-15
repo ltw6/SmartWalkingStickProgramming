76ed5ebda63ee737387690217358d9c24d6b0de2
ltw6 <115619436+ltw6@users.noreply.github.com>
2022-12-15 12:09:49 +0000

    Update ArduinoInterfacing.ino

--- ArduinoInterfacing.ino
+++ ArduinoInterfacing.ino
@@ -1,9 +1,10 @@
 //Variable Initialization
 const int trigPin = 5;
 const int echoPin = 18;
+const int motorPin = 12;
 
 long duration;
-float distanceSm;
+float distanceCm;
 float distanceInch;
 
 //Definitions 
@@ -11,15 +12,37 @@ float distanceInch;
 #define CM_TO_INCH 0.393701
 
 void setup() {
-  //Initialize HCSR05 sensor input pins
-  Serial.begin(9600); //Begin serial communication
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin,INPUT);
-  
- 
-}
+  Serial.begin(115200); // Starts the serial communication
+  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
+  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
+  }
 
 void loop() {
   // put your main code here, to run repeatedly:
+// Clears the trigPin
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  analogWrite( 12 , 600);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(echoPin, HIGH);
   
+  // Calculate the distance
+  distanceCm = duration * SOUND_SPEED/2;
+  
+  // Convert to inches
+  distanceInch = distanceCm * CM_TO_INCH;
+  
+  // Prints the distance in the Serial Monitor
+  Serial.print("Distance (cm): ");
+  Serial.println(distanceCm);
+  Serial.print("Distance (inch): ");
+  Serial.println(distanceInch);
+  analogWrite( 12 , 0 );
+  
+  delay(1000);
 }
